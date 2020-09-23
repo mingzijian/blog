b6e13cbd8e77d9ea3c5e89b2f3e1a8e9e3bcbe7e
mingzijian <1119495352@qq.com>
2020-09-23 19:40:09 +0800

    简单工厂模式(Simple Factory Pattern)

--- Java/DesignPattern/design-pattern-code/src/com/ming/demo/dp/creational/factory/SimpleFactoryPatternDemo.java
+++ Java/DesignPattern/design-pattern-code/src/com/ming/demo/dp/creational/factory/SimpleFactoryPatternDemo.java
@@ -7,4 +7,33 @@ package com.ming.demo.dp.creational.factory;
  */
 public class SimpleFactoryPatternDemo {
 
+    public Movable create(String name){
+        Movable m;
+        switch (name) {
+            case "tank":
+                m=new Tank();
+                break;
+            case "plane":
+                m=new Plane();
+                break;
+            case "cat":
+                m=new Cat();
+                break;
+            case "dog":
+                m=new Dog();
+                break;
+            default:
+                throw new RuntimeException("wrong name");
+
+        }
+        return m;
+    }
+
+    public static void main(String[] args) {
+        SimpleFactoryPatternDemo d=new SimpleFactoryPatternDemo();
+        d.create("tank").move();
+        d.create("plane").move();
+        d.create("cat").move();
+        d.create("dog").move();
+    }
 }
